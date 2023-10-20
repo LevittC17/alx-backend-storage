2e8aff93911d17c468a6d478273ae76434a1bf7d
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-20 21:26:51 +0000

    storing and retrieving lists from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,32 +11,70 @@ from typing import Union, Callable
 from functools import wraps
 
 
-def call_history(method: Callable) -> Callable:
+def count_calls(method: Callable) -> Callable:
     """
-    Decorator to store the history of inputs and outputs for a function.
+    Decorator to count how many times a method is called
 
-    :param method: The method to be decorated
-    :return: Decorated method
+    :param method: The method to be Decorated
+    :return: The decorated method
     """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        # Get the qualified name of the method
+        """Get the qualified name of the method as key"""
+        key = method.__qualname__
+
+        # Increment the count for the key in Redis
+        self._redis.incr(key)
+
+        # Call the original method and return its result
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs and outputs for
+    a function
+
+    :param method: The method to be Decorated
+    :return: The decorated method
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Get the qualified name of the method"""
         key = method.__qualname__
 
         # Store the input arguments as a string in Redis
-        input_key = f"{key}:inputs"
+        input_key = f'{key}:inputs'
         self._redis.rpush(input_key, str(args))
 
         # Execute the wrapped function to retrieve the output
         output = method(self, *args, **kwargs)
 
         # Store the output in Redis
-        output_key = f"{key}:outputs"
+        output_key = f'{key}:output'
         self._redis.rpush(output_key, output)
 
         return output
 
     return wrapper
+
+
+def replay(func: Callable) -> None:
+    """
+    Display the history of calls of a particular function
+    """
+    key = func.__qualname__
+    inputs = cache._redis.lrange(f'{key}:inputs', 0, -1)
+    outputs = cache._redis.lrange(f'{key}:outputs', 0, -1)
+
+    if inputs:
+        print(f'{key} was called {len(inputs)} times:')
+        for input_args, output in zip(inputs, outputs):
+            # Convert the string back to a tuple
+            input_args = eval(input_args.decode('utf-8'))
+            print(f"{key}(*{input_args}) -> {output.decode('utf-8')}")
 
 
 class Cache:
@@ -47,12 +85,12 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in Redis and return the key
 
-        :param data: Data to be stored (str, bytes, int, or float)
+        :param data: Data to be stored (str, bytes, int or float)
         :return: Randomly generated key used for storage
         """
         key = str(uuid.uuid4())
@@ -60,31 +98,37 @@ class Cache:
             self._redis.set(key, data)
             return key
         else:
-            raise ValueError("Data must be str, bytes, int, or float")
+            raise ValueError('Data must be str, bytes, int, or float')
 
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes,
+                                                          int, float,
+                                                          None]:
+        """
+        Get data from Redis and optionally convert it using a callable
 
-def replay(func: Callable) -> None:
-    """
-    Display the history of calls of a particular function.
+        :param key: Key to retrieve data from Redis
+        :param fn: Callable to convert the data (optionally)
+        :return: Retrieved and optionally converted data
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
 
-    :param func: The function to replay the history for
-    """
-    key = func.__qualname__
-    inputs = cache._redis.lrange(f"{key}:inputs", 0, -1)
-    outputs = cache._redis.lrange(f"{key}:outputs", 0, -1)
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Get a string from Redis
 
-    if inputs:
-        print(f"{key} was called {len(inputs)} times:")
-        for input_args, output in zip(inputs, outputs):
-            input_args = eval(input_args.decode("utf-8"))
-            print(f"{key}(*{input_args}) -> {output.decode('utf-8')}")
+        :param key: Key to retrieve a string from Redis
+        :return: Retrieved string or None
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
 
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Get an integer from Redis
 
-if __name__ == "__main__":
-    cache = Cache()
-
-    s1 = cache.store("foo")
-    s2 = cache.store("bar")
-    s3 = cache.store(42)
-
-    replay(cache.store)
+        :param key: Key to retrieve an integer from Redis
+        :return: Retrieved integer or None
+        """
+        return self.get(key, fn=int)
