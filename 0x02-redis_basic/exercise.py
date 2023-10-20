75303f2c88748dd2586531d9ca2546991dae0f11
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-20 10:28:07 +0000

    decorator to store the history of inputs and outputs for a particular function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -60,3 +60,20 @@ class Cache:
             return key
         else:
             raise ValueError('Data must be str, bytes, int or float')
+
+
+if __name__ == "__main__":
+    cache = Cache()
+
+    s1 = cache.store('first')
+    print(s1)
+    s2 = cache.store('second')
+    print(s2)
+    s3 = cache.store('third')
+    print(s3)
+
+    inputs = cache._redis.lrange(f'{cache.store.__qualnamr__}:inputs', 0, -1)
+    outputs = cache._redis.lrange(f'{cache.store.__qualname__}:outputs', 0, -1)
+
+    print('inputs: {}'.format(inputs))
+    print('outputs: {}'.format(outputs))
