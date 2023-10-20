8f6afb135bd477f95ad166bdfbd56402ddfd3a24
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-20 10:21:20 +0000

    decorator to store the history of inputs and outputs for a particular function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,36 +11,42 @@ from typing import Union, Callable
 from functools import wraps
 
 
-def count_calls(method: Callable) -> Callable:
+def call_history(method: Callable) -> Callable:
     '''
-    Decorator to count how many times a method is called
+    Decorator to store the history of inputs and outputs for
+    a function
 
     :param method: The method to be decorated
     :return: Decorated method
     '''
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        '''Get the qualified name of the method as key'''
+    def wrapper(self, *args, **Kwargs):
+        '''Get the qualified name of the method'''
         key = method.__qualname__
 
-        # Increment the count for the key in Redis
-        self._redis.incr(key)
+        # Store the input arguments as a string in Redis
+        input_key = f'{key}:inputs'
+        self._redis.rpush(input_key, str(args))
 
-        # Call the original method and return its result
-        return method(self, *args, **kwargs)
+        # Execute the wrapped function to retrieve the output
+        output = method(self, *args, **kwargs)
+
+        # Store the output in Redis
+        output_key = f'{key}:output'
+        self._redis.rpush(output_key, output)
+
+        return output
 
     return wrapper
 
 
 class Cache:
     def __init__(self):
-        '''
-        Cache class to interact with Redis
-        '''
+        '''Cache class to interact with Redis'''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         Store data in Redis and return the key
@@ -53,48 +59,4 @@ class Cache:
             self._redis.set(key, data)
             return key
         else:
-            raise ValueError('Data must be str, bytes, int, or float')
-
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes,
-                                                          int, float,
-                                                          None]:
-        '''
-        Get data from Redis and optionally convert it using a callable
-
-        :param key: Key to retrieve data from Redis
-        :param fn: Callable to convert the data (optionally)
-        :return: Retrieved and optionally converted data
-        '''
-        data = self._redis.get(key)
-        if data is not None and fn is not None:
-            return fn(data)
-        return data
-
-    def get_str(self, key: str) -> Union[str, None]:
-        '''
-        Get a string from Redis
-
-        :param key: Key to retrieve a string from Redis
-        :return: Retrieved string or None
-        '''
-        return self.get(key, fn=lambda d: d.decode('utf-8'))
-
-    def get_int(self, key: str) -> Union[int, None]:
-        '''
-        Get an intefer from Redis
-
-        :param key: Key to retrieve an integer from Redis
-        :return: Retrieved integer or None
-        '''
-        return self.get(key, fn=int)
-
-
-if __name__ == "__main__":
-    cache = Cache()
-
-    cache.store(b'first')
-    print(cache.get(cache.store.__qualname__))
-
-    cache.store(b'second')
-    cache.store(b'third')
-    print(cache.get(cache.store.__qualname__))
+            raise ValueError('Data must be str, bytes, int or float')
